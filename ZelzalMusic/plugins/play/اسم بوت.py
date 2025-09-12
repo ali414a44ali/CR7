1e480b5cecb6ea111f9bb8ca737019e339ce6d90
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2025-09-12 03:56:51 +0300

    Update اسم بوت.py

--- ZelzalMusic/plugins/play/اسم بوت.py
+++ ZelzalMusic/plugins/play/اسم بوت.py
@@ -1,29 +1,31 @@
 import re
-import asyncio
-from ZelzalMusic import app 
 import random
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from ZelzalMusic import app 
+from pyrogram import filters
 from config import BOT_NAME
 
 Nb = BOT_NAME
 
 italy = [
-         "لبيه وش اغني لك",
-         "اصعد مكالمه {nameuser}",
-         "لا تشغلني اصعد مكالمه",
-         "قول {BOT_NAME} شغل احبك",
-         "قول {BOT_NAME} ابحث احبك",
-         "اغني في قروب ثاني 🦦.",
-         "عيون {BOT_NAME} ايش تحب اسمعك",
-         "ادري عاجبك اسمي ❤️",
-         "يارب يكون شي مهم",
-         ]
+    "لبيه 🫶 وش اغني لك",
+    "اصعد مكالمه {nameuser} ☎️",
+    "لا تشغلني اصعد مكالمه 🙄",
+    "قول `{BOT_NAME} شغل احبك` 💌",
+    "قول `{BOT_NAME} ابحث احبك` 🔍",
+    "اغني في قروب ثاني 🦦",
+    "عيون {BOT_NAME} 👀 ايش تحب اسمعك",
+    "ادري عاجبك اسمي ❤️",
+    "يارب يكون شي مهم 🤔",
+]
 
 @app.on_message(filters.regex(r"^(" + re.escape(Nb) + r")$"))
-
 async def Italymusic(client, message):
     if Nb in message.text:
         response = random.choice(italy)
         response = response.format(nameuser=message.from_user.first_name, BOT_NAME=BOT_NAME)
-        await message.reply(response)
+        
+        # إضافة رابط القناة في الأسفل مع تنسيق ماركداون
+        channel_link = "https://t.me/shahmplus"
+        formatted_response = f"{response}\n\n[✨ 𝐒𝐇𝐀𝐇𝐌 ✨]({channel_link})"
+        
+        await message.reply(formatted_response, disable_web_page_preview=True)
